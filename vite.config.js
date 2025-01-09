ae6d2ace7ec2af83a24db22ad8b3911bb6a99985
kishore <kishorekumar.loganathan@techmobius.com>
2025-01-09 13:44:51 +0530

    build 3

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/tjmDemo/',
     build: {
         outDir: 'docs', // Specify the output directory as "docs"
       },
