9c3185337c4c244df49149ef01ddc1fac52e1e9e
kishore <kishorekumar.loganathan@techmobius.com>
2025-01-09 13:34:49 +0530

    build 2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'docs', // Specify the output directory as "docs"
+      },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
